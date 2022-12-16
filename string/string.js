9d285e70df389d2703e193f3972ca20271ce96c7
DEVLOPERSARWAR620512 <117239709+DEVLOPERSARWAR620512@users.noreply.github.com>
2022-12-15 20:24:45 -0800

    Add files via upload

---
+++ string/string.js
@@ -0,0 +1,12 @@
+var a = " hello ";
+var b = a.charAt(1);
+var c = a.charCodeAt(2);
+var d = a.indexOf("l");
+var e = a.match('h');
+var f = a.replace('h','g');
+var g = a.length;
+var h = a.trim().toLowerCase();
+var i = a.trim().toUpperCase();
+var j = ("hello" === "hello");
+
+console.log(a,b,c,d,e,f,g,h,i,j);
